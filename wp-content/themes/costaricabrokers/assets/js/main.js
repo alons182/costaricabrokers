f6068226b16b7b482f0f039cca05cc47ac301c2d
Alonso Avotz <alonso@avotz.com>
2017-04-25 10:48:52 -0600

    se agrego menu contact us

--- wp-content/themes/costaricabrokers/assets/js/main.js
+++ wp-content/themes/costaricabrokers/assets/js/main.js
@@ -12,6 +12,27 @@
             menu.toggle();
            
         });
+      
+      $('#menu-item-520 a').magnificPopup({ // contact us
+        type: 'inline',
+        midClick: true,
+        removalDelay: 500, //delay removal by X to allow out-animation
+        callbacks: {
+            beforeOpen: function() {
+
+                this.st.mainClass = 'mfp-zoom-out';
+                $('body').addClass('mfp-open');
+            },
+            beforeClose: function() {
+
+               
+                $('body').removeClass('mfp-open');
+            }
+
+        }
+
+       
+    });
       $('.call__btn').magnificPopup({ // contact us
         type: 'inline',
         midClick: true,

411fb57a91a6c2320cf6d5662e5fa94e275b7ba2
Alonso Avotz <alonso@avotz.com>
2017-06-13 12:45:00 -0600

    se agrego taxonomy type

--- wp-content/themes/costaricabrokers/taxonomy-type.php
+++ wp-content/themes/costaricabrokers/taxonomy-type.php
@@ -47,7 +47,7 @@ get_header(); ?>
 								<?php if ( has_post_thumbnail() ) :
 
                                                   $id = get_post_thumbnail_id($post->ID);
-                                                  $thumb_url = wp_get_attachment_image_src($id,'large', true);
+                                                  $thumb_url = wp_get_attachment_image_src($id,'medium', true);
                                                   ?>
                                                   
                                                

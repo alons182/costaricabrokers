16a76b53f92871f89f7c512f30e7eb4ea5225838
Alonso Avotz <alonso@avotz.com>
2017-06-14 12:32:29 -0600

    style diseño del blog

--- wp-content/themes/costaricabrokers/single.php
+++ wp-content/themes/costaricabrokers/single.php
@@ -22,20 +22,21 @@ get_header(); ?>
 			else : ?>
 				<div class="blog-inner">
 					<div class="blog-content">
-						get_template_part( 'template-parts/content', get_post_format() );
+						<?php  get_template_part( 'template-parts/content', get_post_format() ); 
+						the_post_navigation();
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;?>
+
 					</div>
 					<div class="blog-sidebar">
 						<?php get_sidebar(); ?>
 					</div>
 				</div>
 			<?php
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
+			
 			endif;
 
 		endwhile; // End of the loop.

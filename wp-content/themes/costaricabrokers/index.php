3b077b92a4e928ceb9cf39d4439ed7227cf37ff9
Alonso Avotz <alonso@avotz.com>
2017-06-14 12:10:20 -0600

    fix content post for news

--- wp-content/themes/costaricabrokers/index.php
+++ wp-content/themes/costaricabrokers/index.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div class="inner">
 
 		<?php
 
@@ -49,7 +49,7 @@ get_header(); ?>
 
 		endif; ?>
 
-		</main><!-- #main -->
+		</div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
